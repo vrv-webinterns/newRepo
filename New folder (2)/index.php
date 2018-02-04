c854492929eec469e05f82bd00a49b83511b4d6e
Pratheek gogate <35677993+Pratheek10@users.noreply.github.com>
2018-02-04 10:05:14 +0530

    Add files via upload

---
+++ New folder (2)/index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+	<head>
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width = device-width, initial-scale = 1">
+		<title>Example</title>
+		<link rel = "stylesheet" type = "text/css" href = "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.min.css">
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap-theme.min.css">
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css">
+		<script type="text/javascript" src = "js/jquery.min.js"></script>
+		<script type="text/javascript" src = "js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		
+		<div class="container-fluid" style="background-color:dodgerblue">
+			
+			
+			<div class="row" style="background-color:dodgerblue">
+				<div class="jumbotron text-center" style="background-color:dodgerblue;margin-top:80px;margin-bottom:100px;">
+					<form>
+						
+						<h2 style="color:green">GO AHEAD GET INTENSHIP</h2>
+						<div class="col-sm-4 col-sm-offset-4">
+							<div class="form-group">
+								<input type="text" class="form-control">
+							</div>
+						</div>
+						<div class="button">
+							<button class="btn btn-primary"style="float:left;margin:-14.1px;margin-top:0px;">GO</button>
+						</div>
+					</form>
+					
+				</div>
+			</div>
+			
+					
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container"style="background-color:dodgerblue">
+		<div class="nav-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" style="color:white;" href="#" ><span class="glyphicon glyphicon-education" ></span>&nbsp;SHIKSHAN<sub>internship that matters</sub></a>
+		</div>
+			<div class="collapse navbar-collapse" id="myNavbar"style="color:white;">
+				<ul class="nav navbar-nav navbar-right"style="color:white;">
+				  
+				 <li><a href="logina.html"style="color:white;">LOGIN</a></li>
+				 <li><a href="registrationform.html"style="color:white;">REGISTER</a></li>
+				 <li><a href="dummyabout.html"style="color:white;">CONTACT</a></li>
+				 <li><a href="ab.html"style="color:white;">ABOUT US</a></li>
+				</ul>
+			</div>
+	</div>
+</nav>
+		 <div class="popular-message"><h1 style="color:yellow;">Internships in popular categories</h1></div>
+        <a href="">
+            <div style="background-image: url('/static/images/home/discover/category_engineering_new.jpg');" class="discover-image">
+               
+            </div>
+        </a>
+
+        <a href="">
+		<div class="col-sm-3" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/cse.jpg"style="width:200px;height:200px" >
+               </div> <span href="engi.html"><h1>Engineering</h1></span>
+			   <div class="panel-footer">
+					<h5>CS/EC/EE</h5>
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<small>&nbsp;(1845 reviews)</small><br>
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+ <a href="#">
+		<div class="col-sm-3" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/degree.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Degree</h1></span>
+			   <div class="panel-footer">
+					<h5>BSC/BBA/MSC</h5>
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<small>&nbsp;(1845 reviews)</small><br>
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+ <a href="#">
+		<div class="col-sm-3" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/mba.jpg"style="width:200px;height:210px" >
+               </div> <span href="#"><h1>MBA</h1></span>
+			   <div class="panel-footer">
+					<h5>Business/SE<h5>
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<small>&nbsp;(1845 reviews)</small><br>
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+<a href="#">
+		<div class="col-sm-3" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/diploma.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Diploma</h1></span>
+			   <div class="panel-footer">
+					<h5>CS/EC/EE</h5>
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<small>&nbsp;(1845 reviews)</small><br>
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+           		 <div class="popular-message"><h1 style="color:yellow;">Internships in popular Cities</h1></div>
+        <a href="">
+            <div style="background-image: url('/static/images/home/discover/category_engineering_new.jpg');" class="discover-image">
+                
+            </div>
+        </a>
+
+        <a href="#">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/bangalore.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Bangalore</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+	        <a href="#">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/mangalore.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Mangalore</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+		        <a href="">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/mysore.jpg" style="width:200px;height:200px">
+               </div> <span href="#"><h1>Mysore</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+		        <a href="">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/a.jpg" style="width:200px;height:200px">
+               </div> <span href="#"><h1>Mumbai</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+		        <a href="#">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/chennai.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Chennai</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+		        <a href="#">
+		<div class="col-sm-4" >
+			<div class= "panel">
+			<div class="panel-body">
+            <img class="img-responsive center-block" src="images/hydrabad.jpg"style="width:200px;height:200px" >
+               </div> <span href="#"><h1>Hydrabad</h1></span>
+			   <div class="panel-footer">
+					<!-- <h5>CS/EC/EE</h5> -->
+					<p>&#8377;<span class="strikedprice">10000</span><span class="price"><strike>&nbsp;&#8377;25000</strike></span>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star fill"aria-hidden="true"></i>
+					<i class="fa fa-star"aria-hidden="true"></i>
+					<i class="fa fa-star" aria-hidden="true"></i>
+					<!-- <small>&nbsp;(1845 reviews)</small><br> -->
+					<a href="#">view more about intenship</a>
+            </div>
+			</div>
+			</div>
+        </a>
+			</div>				
+					
+					
+		<script type="text/javascript" src = "js/script.js"></script>
+	</body>
+</html>
